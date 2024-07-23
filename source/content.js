c06419b55bfe6f17912c678a8ed0d621dc436e06
Joseph Petersen <me@jetersen.dev>
2024-07-23 19:29:28 +0200

    fix formatting

--- source/content.js
+++ source/content.js
@@ -1,20 +1,20 @@
 async function init() {
-	// get all divs with timesheet entries excluding weekends
-	let divs = document.querySelectorAll('div.TimesheetSlat:not(.js-timesheet-showWeekends)');
-	// exclude divs with extra info items most likely to be sick days or holidays
-	let filteredDivs = Array.from(divs).filter(x => !x.querySelector('.TimesheetSlat__extraInfoItem'));
-	let nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, "value").set;
-	let inputEvent = new Event('input', { bubbles: true });
+	// Get all divs with timesheet entries excluding weekends
+	const divs = document.querySelectorAll('div.TimesheetSlat:not(.js-timesheet-showWeekends)');
+	// Exclude divs with extra info items most likely to be sick days or holidays
+	const filteredDivs = Array.from(divs).filter(x => !x.querySelector('.TimesheetSlat__extraInfoItem'));
+	const nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, 'value').set;
+	const inputEvent = new Event('input', {bubbles: true});
 	let inputChanged = false;
-	filteredDivs.forEach(x => {
-		let input = x.querySelector('.TimesheetSlat__input');
-		// exclude input if it is disabled or has a value
+	for (const x of filteredDivs) {
+		const input = x.querySelector('.TimesheetSlat__input');
+		// Exclude input if it is disabled or has a value
 		if (input && !input.disabled) {
 			nativeInputValueSetter.call(input, '7,4');
 			input.dispatchEvent(inputEvent);
 			inputChanged = true;
 		}
-	});
+	}
 
 	if (inputChanged) {
 		document.dispatchEvent(new CustomEvent('SiteFooter:open'));
